ec6ceda717688bfff82946d191ebe190e96edebb
dev-vickie <vmutethia84@gmail.com>
2022-12-05 21:23:30 +0300

    initial commit

--- lib/responsive/constant.dart
+++ lib/responsive/constant.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 var myAppBar = AppBar(
   backgroundColor: Colors.grey[900],
 );
@@ -8,40 +9,35 @@ var myDrawer = Drawer(
   child: Column(
     children: [
       DrawerHeader(
-        padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 45),
                 child: Image.asset(
-                    'assets/images/dvla.png',
+                  'assets/images/dvla.png',
                 ),
               ),
               const Text(
-                  'D V L A',
-                style: TextStyle(
-                    fontSize: 24,
-                  fontWeight: FontWeight.bold
-                ),
+                'D V L A',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
             ],
-          )
-      ),
-
-      ListTile(
+          )),
+      const ListTile(
         leading: Icon(Icons.home),
         title: Text('D A S H B O A R D'),
       ),
-      ListTile(
+      const ListTile(
         leading: Icon(Icons.add_card),
         title: Text('V I S I T O R S'),
       ),
-      ListTile(
+      const ListTile(
         leading: Icon(Icons.settings),
         title: Text('S E T T I N G S'),
       ),
-      ListTile(
+      const ListTile(
         leading: Icon(Icons.logout),
         title: Text('L O G O U T'),
       ),
